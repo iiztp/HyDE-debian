3dafb61a6ac1e0cff9c937697fa93b5bd2838957
iiztp <amet.matthieu@gmail.com>
2025-10-13 22:47:57 +0200

    Added dep

--- Scripts/installers/git-make.sh
+++ Scripts/installers/git-make.sh
@@ -7,7 +7,7 @@ version=$3
 if [ ! -d "${name}" ] ; then
     git clone --depth 1 --branch ${version} --recursive ${link} ${name}
 fi
-
+source $HOME/.cargo/env
 cd "${name}"
 git checkout ${version}
 
